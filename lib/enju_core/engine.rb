f650ce0c027f22323c92dc9dbd062fcd28aaec23
Kosuke Tanabe <kosuke@e23.jp>
2014-01-07 16:36:46 +0900

    require redis/server

--- lib/enju_core/engine.rb
+++ lib/enju_core/engine.rb
@@ -6,7 +6,7 @@ require 'attribute_normalizer'
 require 'friendly_id'
 require 'addressable/uri'
 require 'sunspot_rails'
-require 'resque'
+require 'resque/server'
 require 'settingslogic'
 require 'nested_form'
 
