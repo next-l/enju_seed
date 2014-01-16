4f8de59a5e69802bcc2a4879e4308fff8a2d34ea
Kosuke Tanabe <kosuke@e23.jp>
2014-01-17 03:40:13 +0900

    do not require protected_attributes

--- lib/enju_seed/engine.rb
+++ lib/enju_seed/engine.rb
@@ -9,7 +9,7 @@ require 'sunspot_rails'
 require 'resque/server'
 require 'settingslogic'
 require 'nested_form'
-require 'protected_attributes'
+#require 'protected_attributes'
 
 module EnjuSeed
   class Engine < ::Rails::Engine
