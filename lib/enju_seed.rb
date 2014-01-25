39182c728581403ef285575e9709d3b6cb3c4c0f
Kosuke Tanabe <kosuke@e23.jp>
2014-01-26 03:56:00 +0900

    fixed loading modules

--- lib/enju_seed.rb
+++ lib/enju_seed.rb
@@ -1,4 +1,12 @@
 require "enju_seed/engine"
+require "enju_seed/bookmark_url"
+require "enju_seed/calculate_stat"
+require "enju_seed/expire_editable_fragment"
+require "enju_seed/expire_fragment_cache"
+require "enju_seed/import_file"
+require "enju_seed/localized_name"
+require "enju_seed/master_model"
+require "enju_seed/url_validator"
 
 module EnjuSeed
 end
--- lib/enju_seed/version.rb
+++ lib/enju_seed/version.rb
@@ -1,3 +1,3 @@
 module EnjuSeed
-  VERSION = "0.2.0.pre3"
+  VERSION = "0.2.0.pre4"
 end
