6c5ab4c64b5dc66a145fcb8cda4d57073a89cbd6
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-24 14:59:59 +0900

    moved modules to enju_leaf

--- lib/enju_seed.rb
+++ lib/enju_seed.rb
@@ -1,12 +1,5 @@
 require "enju_seed/engine"
-require "enju_seed/bookmark_url"
-require "enju_seed/calculate_stat"
-require "enju_seed/expire_editable_fragment"
-require "enju_seed/expire_fragment_cache"
-require "enju_seed/import_file"
-require "enju_seed/localized_name"
-require "enju_seed/master_model"
-require "enju_seed/url_validator"
+require "enju_seed/version"
 
 module EnjuSeed
 end
