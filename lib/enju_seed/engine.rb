66c33f229775a0f6baff5511890ec64887bb43c3
Kosuke Tanabe <nabeta@fastmail.fm>
2016-07-31 20:17:37 +0900

    bump version

--- lib/enju_seed/engine.rb
+++ lib/enju_seed/engine.rb
@@ -1,16 +1,3 @@
-require 'kaminari'
-require 'devise'
-require 'acts_as_list'
-require 'attribute_normalizer'
-require 'friendly_id'
-require 'addressable/uri'
-require 'elasticsearch/model'
-require 'elasticsearch/rails'
-require 'resque/server'
-require 'settingslogic'
-require 'nested_form'
-require 'pundit'
-
 module EnjuSeed
   class Engine < ::Rails::Engine
   end
