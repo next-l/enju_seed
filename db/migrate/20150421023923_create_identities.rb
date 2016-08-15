4782e686133db9b462383e47b46b3aa961ea01c3
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-15 21:02:58 +0900

    update migration files

--- db/migrate/20150421023923_create_identities.rb
+++ db/migrate/20150421023923_create_identities.rb
@@ -4,12 +4,11 @@ class CreateIdentities < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
-      t.integer :profile_id
+      t.references :profile, index: true
 
       t.timestamps null: false
     end
     add_index :identities, :name
     add_index :identities, :email
-    add_index :identities, :profile_id
   end
 end
