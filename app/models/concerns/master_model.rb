4bcd210c98acc8b4f92c41f8b8c1d05037a05a78
Kosuke Tanabe <nabeta@fastmail.fm>
2017-01-13 02:05:10 +0900

    remove valid_yaml?

--- app/models/concerns/master_model.rb
+++ app/models/concerns/master_model.rb
@@ -3,13 +3,9 @@ module MasterModel
 
   included do
     acts_as_list
-    validates :name, uniqueness: { case_sensitive: false }
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
     validate :name do
       valid_name?
-    end
-    validate :display_name do
-      valid_yaml?
     end
     validates :display_name, presence: true
     before_validation :set_display_name, on: :create
@@ -27,11 +23,5 @@ module MasterModel
     unless name =~ /\A[a-z][0-9a-z_]*[0-9a-z]\z/
       errors.add(:name, I18n.t('page.only_lowercase_letters_and_numbers_are_allowed'))
     end
-  end
-
-  def valid_yaml?
-    YAML.load(display_name)
-  rescue Psych::SyntaxError
-    errors.add(:display_name, I18n.t('page.cannot_parse_yaml_header'))
   end
 end
