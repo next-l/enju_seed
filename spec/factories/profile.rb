d8f30181aaae05524d5077cc39a104fac4b5bb01
Kosuke Tanabe <nabeta@fastmail.fm>
2018-10-06 16:53:47 +0900

    update factory files

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
-  factory :profile, class: Profile do |f|
-    f.user_group_id { UserGroup.first.id }
-    f.required_role_id { Role.find_by(name: 'User').id }
-    f.sequence(:user_number) { |n| "user_number_#{n}" }
-    f.library_id { Library.find_by(name: 'kamata').id }
-    f.locale 'ja'
+  factory :profile, class: Profile do
+    user_group_id { UserGroup.first.id }
+    required_role_id { Role.find_by(name: 'User').id }
+    sequence(:user_number) { |n| "user_number_#{n}" }
+    library_id { Library.find_by(name: 'kamata').id }
+    locale { 'ja' }
     factory :librarian_profile, class: Profile do |profile|
       profile.required_role_id { Role.find_by(name: 'Librarian').id }
       profile.association :user, factory: :librarian
