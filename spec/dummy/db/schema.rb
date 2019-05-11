663529ef27182eff670ba9954678f06be9cd0d33
Kosuke Tanabe <nabeta@fastmail.fm>
2019-05-11 13:19:11 +0900

    add unique index to users

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1082,7 +1082,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_151124) do
     t.datetime "confirmed_at"
     t.bigint "profile_id", null: false
     t.index ["email"], name: "index_users_on_email"
-    t.index ["profile_id"], name: "index_users_on_profile_id"
+    t.index ["profile_id"], name: "index_users_on_profile_id", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
