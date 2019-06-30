3d0d42f669cde83443ad6b4e25d6e7c35fec10ed
Kosuke Tanabe <nabeta@fastmail.fm>
2019-06-30 15:42:03 +0900

    fix default template file

--- db/migrate/041_create_roles.rb
+++ db/migrate/041_create_roles.rb
@@ -2,7 +2,7 @@ class CreateRoles < ActiveRecord::Migration[5.2]
   def change
     create_table "roles" do |t|
       t.string :name, null: false, index: {unique: true}
-      t.jsonb :display_name_translations, default: {}, null: false
+      t.text :display_name
       t.text :note
       t.integer :position, default: 1, null: false
       t.timestamps
