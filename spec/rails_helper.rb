f5765dd23e21030752930186a8c419749bdf1e47
Kosuke Tanabe <nabeta@fastmail.fm>
2020-03-14 21:46:19 +0900

    update spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
-  #ActiveRecord::Migration.maintain_test_schema!
+  ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
